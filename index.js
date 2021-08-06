938fd1669f99f4b3482606c78f9f1e758e2fb998
dairan <dairan103@aliyun.com>
2021-08-06 11:05:38 +0800

    feat: 删除env

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const presetBeforeServeFn = require('./interceptRequest')
-const getAnswers = require('./interactive/index')
 const loadConfig = require('./interceptRequest/load-config.js')
 module.exports = async (api, options) => {
   const { serve, build } = api.service.commands
   const serveFnCache = serve.fn
   const buildFnCache = build.fn
   serve.fn = async (...args) => {
-    const answer = await getAnswers(loadConfig().env)
     presetBeforeServeFn.call(null, api, options, answer)
     return serveFnCache(...args)
   }
